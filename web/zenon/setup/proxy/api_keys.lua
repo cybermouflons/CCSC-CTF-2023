db8589c71b3da4194ba6c86e57e37a5021cc4a28
apogiatzis <apoyiatzis@outlook.com>
2023-07-12 02:59:17 +0100

    web(zenon): remove exposed port from bot

--- web/zenon/setup/proxy/api_keys.lua
+++ web/zenon/setup/proxy/api_keys.lua
@@ -5,4 +5,5 @@ api_keys:add("key2", "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")
 api_keys:add("key3", "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")
 api_keys:add("key4", "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")
 api_keys:add("key5", "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")
-
+api_keys:add("key6", "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")
+api_keys:add("key7", "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")
