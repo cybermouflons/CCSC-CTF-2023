32f393f030b2723feb07ed275496b7ce1403a07b
apogiatzis <apoyiatzis@outlook.com>
2023-07-13 23:49:49 +0100

    web(zenon): wait for console

--- web/zenon/setup/bot/app.py
+++ web/zenon/setup/bot/app.py
@@ -23,40 +23,42 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     )
     return encoded_jwt
 
-
 @app.get("/visit_question")
 async def visit(id: int):
     access_token = create_access_token(data={"sub": "admin"})
-    async with async_playwright() as p:
-        id = int(id)
-        browser = await p.chromium.launch(
-            headless=True,
-            args=[
-                "--verbose",
-                "--no-sandbox",
-            ],
-        )
-        context = await browser.new_context()
-        url = f"http://{settings.APP_HOSTNAME}/"
-        flag_cookie = {
-            "url": url,
-            "name": "flag",
-            "value": "CCSC{LLMs_4nD_w3b_VuLn5_B3cAus3_wHY_n0t!}",
-            "httpOnly": True,
-        }
-        token_cookie = {
-            "url": url,
-            "name": "access_token",
-            "value": access_token,
-            "httpOnly": True,
-        }
-        await context.add_cookies([flag_cookie, token_cookie])
-        page = await context.new_page()
-        url = f"http://{settings.APP_HOSTNAME}:8000/question?id={id}"
-        print("Visiting ", url)
-        await page.goto(url)
-        await page.content()  # triggers javascript
-        await page.wait_for_selector("#navbarNav")
-        print("Loaded")
+    try:
+        async with async_playwright() as p:
+            id = int(id)
+            browser = await p.chromium.launch(
+                headless=True,
+                args=[
+                    "--verbose",
+                    "--no-sandbox",
+                ],
+            )
+            context = await browser.new_context()
+            url = f"http://{settings.APP_HOSTNAME}/"
+            flag_cookie = {
+                "url": url,
+                "name": "flag",
+                "value": "CCSC{LLMs_4nD_w3b_VuLn5_B3cAus3_wHY_n0t!}",
+                "httpOnly": True,
+            }
+            token_cookie = {
+                "url": url,
+                "name": "access_token",
+                "value": access_token,
+                "httpOnly": True,
+            }
+            await context.add_cookies([flag_cookie, token_cookie])
+            page = await context.new_page()
+            url = f"http://{settings.APP_HOSTNAME}:8000/question?id={id}"
+            print("Visiting ", url)
+            async with page.expect_console_message(timeout=5000) as msg_info:
+                await page.goto(url)
+                await page.content()  # triggers javascript
+            print(await msg_info.value)
+    except Exception as e:
+        print("Exception", e)
 
     return {"status": "OK"}
