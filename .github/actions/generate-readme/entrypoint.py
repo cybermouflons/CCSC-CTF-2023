72c7f695b973f5b152e7a3235743c52db339573c
apogiatzis <apoyiatzis@outlook.com>
2023-07-16 11:19:21 +0100

    ci(challops): use jinja

--- .github/actions/generate-readme/entrypoint.py
+++ .github/actions/generate-readme/entrypoint.py
@@ -1,35 +1,33 @@
 import os
 import yaml
 import sys
+from jinja2 import Environment, FileSystemLoader
 
-def generate_challenge_list(directory):
-    challenge_table = "| Name | Author |\n| ---- | ------ |\n"
-    with open(os.path.join(directory, "challenge.yml"), "r") as file:
-        challenge = yaml.load(file, Loader=yaml.FullLoader)
-        challenge_table += f"| [{challenge['name']}]({directory}) | {challenge['author']} |\n"
-    return challenge_table
+def parse_challenge(directory):
+    path = os.path.join(directory, "challenge.yml")
+    print(path)
+    with open(path, "r") as file:
+        return yaml.load(file, Loader=yaml.FullLoader)
 
 def main():
-    directories = sys.argv[1]
-    print("Directories:", directories)
-
+    directories = sys.argv[1].split(' ')
     challenge_categories = {}
+
     for directory in directories:
-        category_table = generate_challenge_list(directory)
-        challenge_categories[directory] = category_table
+        challenge = parse_challenge(directory)
+        category = challenge['category']
+        if category not in challenge_categories:
+            challenge_categories[category] = []
+        challenge_categories[category].append(challenge)
 
-    readme_template = ""
-    with open("README.md", "r") as file:
-        readme_template = file.read()
+    file_loader = FileSystemLoader('/')
+    env = Environment(loader=file_loader)
+    template = env.get_template('README.jinja')
 
-    challenges_section = ""
-    for category, table in challenge_categories.items():
-        challenges_section += f"### {category}\n\n{table}\n\n"
-
-    readme_content = readme_template.replace("{{ challenges_placeholder }}", challenges_section.strip())
+    output = template.render(challenge_categories=challenge_categories)
 
     with open("README.md", "w") as file:
-        file.write(readme_content)
+        file.write(output)
 
 if __name__ == "__main__":
     main()
